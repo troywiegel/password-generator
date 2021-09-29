7efc0bf1f84ed86f1e2b5b2b4c7251662c6389be
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2021-09-28 22:44:56 -0700

    added to var strings

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -28,10 +28,10 @@ function generatePassword() {
   var askNChar;
   var askSChar;
   
-  var lChar = "abcdefghijklmnopqrstuvwxyz";
-  var uChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var nChar = "0123456789";
-  var sChar = "!@#$%^&*()+="
+  var lChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var nChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var sChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "="];
   var password = '';
 
   var askLength = (prompt("How many characters would you like to include?"));
