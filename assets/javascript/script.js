0abfc87fc9887c2e49b5283dc67578e6bfa4e608
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2021-09-28 22:01:37 -0700

    adding comments to map out how the generator will work

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,5 +10,22 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var password = '';
+
+// Ask for number of characters
+  // if the user didn't pick 8 or more, end app with error that password needs to be 8 or more characters
+// Ask yes/no for each type of character (lowercase, uppercase, numeric, special)
+  // if the user says no to all, end app with error that they should pick atleast 1
+// make variables for each var lowercase, uppercase, etc. (can type one big string into variable)
+
+// for the number of characters that the user picked, randomly build the password character by character
+  // generate a random number
+  //grab a randonm character based on that number and append it (concatenate) to var password (password + random character)
+
+
+  return password;
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
